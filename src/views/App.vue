254d2b2ee5030ca90fde99b604a2716e6d8a33f7
LiTee <2056448327@qq.com>
2024-03-16 19:02:27 +0800

    homework1

--- src/views/App.vue
+++ src/views/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import SideBarView from './SideBarView.vue';
+import HomeWork1 from './HomeWorks/Homework1/HomeWork1.vue';
 </script>
 
 <template >
   <SideBarView id="sidebar"/>
   <RouterView id="router"/>
+  <HomeWork1 id="homework"/>
 </template>
 <style >
     #app{
